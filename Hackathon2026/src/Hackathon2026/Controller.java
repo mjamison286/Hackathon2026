03e690320e567aeba90c733024588a998ed3310a
mjamison286 <mjamison286@gmail.com>
2026-04-25 19:57:52 -0500

    created basic ui with no functionality

--- Hackathon2026/src/Hackathon2026/Controller.java
+++ Hackathon2026/src/Hackathon2026/Controller.java
@@ -1,13 +1,19 @@
 package Hackathon2026;
 
+import java.io.File;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.*;;
 
 public class Controller {
 
     @FXML
     private Label label;
+
+    @FXML
+    private ImageView imageViewer;
 
     @FXML
     void test(ActionEvent actionEvent)
@@ -21,5 +27,8 @@ public class Controller {
         String javafxVersion = System.getProperty("javafx.version");
 
         label.setText("JavaFX Version: " + javafxVersion + " and Java Version: " + javaVersion);
+
+        Image imageToSet = new Image(new File("assets\\image\\media-wallpaper-full3.jpg").toURI().toString());
+        imageViewer.setImage(imageToSet);
     }
 }
